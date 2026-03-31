e1246ece012cebe5edb39e3f0b00612988ec8f35
yunchen0625 <yunchen0625@gmail.com>
2026-03-31 09:32:41 +0800

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,9 @@
+def Process(x):
+	x = x.split(" ")
+	school = x[0].replace("我是","")
+	print(f"學校:{school}\n姓名:{x[2]}")
+
+# 只有執行 exampple1.py 時，這段才會跑
+if
+Name = "我是靜宜大學 資管二A 蔡伃捷"
+Process(Name)
---
+++ mis.py
@@ -0,0 +1,4 @@
+from example1 import Process
+
+User = "我是靜宜大學 資管二A yjtsai"
+Process(User)
