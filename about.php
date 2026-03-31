e2a8f9b6941ce6bebe4ce5bf1aaaa6b2e686b6e0
yunchen0625 <yunchen0625@gmail.com>
2026-03-31 09:50:49 +0800

    Update user name in about.php form

--- about.php
+++ about.php
@@ -7,7 +7,7 @@
 <body>
 	<form action="#" method="post">
 		帳號：
-		<input type="text" name="user" value="伃捷">
+		<input type="text" name="user" value="云甄">
 		<br><br>密碼：
 		<input type="password" name="pwd" value="123456">
 		<br><br><input type="submit" value="確定送出">
